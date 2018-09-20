5a28a302c23334c9791aef53a874688c0dd5271f
Jeremy White <patch3s93@gmail.com>
2018-09-20 16:20:02 -0400

    minedminds

---
+++ hello_world.rb
@@ -0,0 +1,9 @@
+count = 1
+until count == 101
+ if count.odd? == true
+ 	    puts "odd"
+    else
+ 	puts count
+    end
+	count = count+1
+end
---
+++ hello_world2.rb
@@ -0,0 +1,16 @@
+count = 1
+until count == 101
+    if count %3 == 0 &&
+        count % 5 == 0
+ 	     puts "mined minds"
+     elsif
+ 	     count %3 == 0
+ 	     puts "mined"
+     elsif 
+        count %5 == 0
+         puts "minds"
+     else
+ 	 puts count
+     end
+	 count = count+1
+end
