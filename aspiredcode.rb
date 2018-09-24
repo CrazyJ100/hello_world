d03b3ea53e4cc68a20c2bacfd5d7414ee0bbd042
Jeremy White <patch3s93@gmail.com>
2018-09-24 18:32:59 -0400

    aspirecode

---
+++ aspiredcode.rb
@@ -0,0 +1,7 @@
+- 1.upto(100) do |x|
+		if x.odd?
+			puts "aspire"
+		else
+			puts x
+	end
+end
---
+++ codinghw.rb
@@ -0,0 +1,16 @@
+count = 1
+until count == 201
+	 if count %4 == 0 &&
+        count %8 == 0
+ 	     puts "hewlett packet"
+     elsif
+ 	     count %4 == 0
+ 	     puts "hewlett"
+     elsif 
+        count %8 == 0
+         puts "packet"
+     else
+ 	 puts count
+     end
+	 count = count+1
+end
