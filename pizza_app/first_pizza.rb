b0398983475d025774ef24307d8ed06bc07bde83
Jeremy White <patch3s93@gmail.com>
2018-10-04 14:36:02 -0400

    add tips, delivery, and cost for the size of the pizza function

--- pizza_app/first_pizza.rb
+++ pizza_app/first_pizza.rb
@@ -9,7 +9,7 @@ def sauce
 end
 def cheese 
 	cheese = ["Sharp Bacon Cheddar", "Extra Cheese", "Mozzerella", "Parmesean"].sample
-end
+end 
 def toppings
 	toppings = ["Pepperoni","Chicken", "Bacon", "Bacon", "Sausage"].sample
 end
@@ -22,41 +22,79 @@ end
 def delivery
 	delivery = ["5 miles","10 miles","15 miles","20 miles"].sample
 end
+def cost
+	cost =["$5","$9","$13","$15","18"].sample
+end 
 
 
 
 def my_pizza(size, crust, sauce, cheese, toppings, veggies, cook)
-	puts "size: #{size}"
+	puts "__________________"
+	puts "size is #{size}."
+	if size == "14 inch"
+		$cost_size += 13
+	elsif  size == "9 inch"
+		$cost_size += 5
+	elsif 
+		size == "11 inch"
+		$cost_size += 9
+	elsif
+		size == "16 inch"
+		$cost_size += 15
+	elsif 
+		size == "20 inch"
+		$cost_size += 18
+	end 
+	puts "cost_size #{$cost_size}"
+	# puts "size: #{size}"
 	puts "crust: #{crust}"
 	puts "sauce: #{sauce}"
 	puts "cheese: #{cheese}"
 	puts "toppings: #{toppings}"
 	puts "veggies: #{veggies}"
 	puts "cook: #{cook}"
+	# puts "cost: #{cost}"
 	puts "__________________"
 end
-
-
-
+$cost_size = 0
 print "How many pizzas would you like?"
 count = gets.chomp.to_i
-price = count * 6
+# price = count * 6
+pizza_count = 0
 delivery_price = 0
-del = delivery 
+del = delivery
+# cost_size = 0
+
+
 count.times do
+	puts "pizza number: #{pizza_count += 1}"
 	my_pizza(size, crust, sauce, cheese, toppings, veggies, cook)
 end
 if del == "5 miles"
-	puts "under 5 miles"
+	puts "under 5 miles for delivery"
 elsif 
 	del == "10 miles"
-	puts "10 miles"
+	puts "10 miles for delivery"
 	delivery_price += 5
-elsif del == "15 miles"
-	puts "15 miles"
+elsif 
+	del == "15 miles"
+	puts "15 miles for delivery"
 	delivery_price += 7
-elsif del == "20 miles"
-	puts "20 miles"
+elsif 
+	del == "20 miles"
+	puts "20 miles for delivery"
 	delivery_price += 9
 end
-puts "Delivery cost is #{delivery_price}, so total cost is #{delivery_price + price}."
+# print "$cost_size #{$cost_size}"
+# print "delivery_price #{delivery_price}"
+puts "You wanted #{count} pizza?"
+tip = ($cost_size + delivery_price) * 0.15
+print "It's gonna cost #{$cost_size} before delivery."
+puts "tips is #{tip}."
+# total_cost = 
+
+
+
+puts "Delivery cost is #{delivery_price}. So your total cost is gonna be #{delivery_price + tip + $cost_size}."
+
+# puts "Your total is #{delivery_price + cost}."
